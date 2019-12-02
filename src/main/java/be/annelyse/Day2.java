43d3398a1ec6a692b93f0f21414ba8a452c0117d
Annelies Claes <annelies.claes@vrt.be>
2019-12-02 11:24:15 +0100

    --- Day 2: 1202 Program Alarm --- (part2)

--- src/main/java/be/annelyse/Day2.java
+++ src/main/java/be/annelyse/Day2.java
@@ -9,70 +9,89 @@ import java.util.stream.Collectors;
 
 public class Day2 {
 
-
     public static void main(String[] args) throws IOException {
-        String input = Files.readString(Paths.get("src/main/resources/input", "Day2"));
-        List<Integer> intCodeInput = Arrays.stream(input.split(","))
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
-
         System.out.println("\n********************************************Part1********************************************");
-        List<Integer> intCodeInitialized = initialize(intCodeInput, 1, 12, 2, 2);
-        solvePart1(intCodeInitialized);
+        System.out.println("\nSolution part 1: " + solvePart1(getIntCodeInput(), 1202));
 
-
+        System.out.println("\n********************************************Part2********************************************");
+        System.out.println("\nSolution part 2: " + solvePart2(19690720));
     }
 
-    static List<Integer> solvePart1(List<Integer> input) {
+    static List<Integer> getIntCodeInput() throws IOException {
+        String input = Files.readString(Paths.get("src/main/resources/input", "Day2"));
+        return Arrays.stream(input.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+    static int solvePart1(List<Integer> intCodeinput, int computerCode) {
+        List<Integer> initializedInputCode = initializeWithComputerCode(intCodeinput, computerCode);
+        List<Integer> transformedIntCode = solvingAlgoritm(initializedInputCode);
+        return transformedIntCode.get(0);
+    }
+
+    static List<Integer> solvingAlgoritm(List<Integer> input) throws IndexOutOfBoundsException {
 
         List<Integer> intCode = input;
         int opCode = intCode.get(0);
-        for (int i = 0; opCode != 99; i = i + 4) {
+        for (int instructionPointer = 0; opCode != 99; ) {
             if (opCode == 1) {
-                intCode = optcode1(intCode, i);
+                intCode = optcode1(intCode, instructionPointer);
             } else if (opCode == 2) {
-                intCode = optcode2(intCode, i);
+                intCode = optcode2(intCode, instructionPointer);
             }
-            opCode = intCode.get(i + 4);
+            opCode = intCode.get(instructionPointer += 4);
         }
+        return intCode;
+    }
 
-        System.out.println("\nThe result is: ");
-        intCode.forEach(value -> System.out.print(value + ", "));
+    static int solvePart2(Integer output) throws IOException {
+        for (int i = 0; i < 10000; i++) {
+            int result;
+            List<Integer> input = getIntCodeInput();
+            try {
+                result = solvePart1(input, i);
+                System.out.println("iteratie "+i +": " + result);
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("iteratie "+i +": " + e.getMessage());
+                result = 88888888;
+            }
+            if (result == output) {
+                return i;
+            }
+        }
+        return 666666;
+    }
+
+
+    static List<Integer> optcode1(List<Integer> intCode, int position) throws IndexOutOfBoundsException {
+
+        int noun = intCode.get(intCode.get(position + 1));
+        int verb = intCode.get(intCode.get(position + 2));
+        intCode.set(intCode.get(position + 3), noun + verb);
 
         return intCode;
     }
 
-    static void solvePart2(List<Integer> input) {
-
-    }
-
-
-    static List<Integer> optcode1(List<Integer> intCode, int position) {
-
+    static List<Integer> optcode2(List<Integer> intCode, int position) throws IndexOutOfBoundsException {
         int value1 = intCode.get(intCode.get(position + 1));
         int value2 = intCode.get(intCode.get(position + 2));
-        intCode.set(intCode.get(position + 3), value1 + value2);
+        intCode.set(intCode.get(position + 3), value1 * value2);
 
         return intCode;
     }
 
-    static List<Integer> optcode2(List<Integer> intCode, int position) {
-        int value1 = intCode.get(intCode.get(position + 1));
-        int value2 = intCode.get(intCode.get(position + 2));
-        int result = intCode.set(intCode.get(position + 3), value1 * value2);
+    static List<Integer> initializeWithComputerCode(List<Integer> intCode, Integer computerCode) {
+
+        int noun = computerCode / 100;
+        int verb = computerCode % 100;
+
+        intCode.set(1, noun);
+        intCode.set(2, verb);
 
         return intCode;
     }
 
-    static List<Integer> initialize(List<Integer> intCode, Integer... initializationPositionAndValue) {
 
-        for (int i = 0; i < initializationPositionAndValue.length; i = i + 2) {
-            //cast naar int, anders wordt de waarde 1 verwijderd ipv de index 1 !!!!
-            intCode.remove((int) initializationPositionAndValue[i]);
-            intCode.add(initializationPositionAndValue[i], initializationPositionAndValue[i + 1]);
-        }
-
-        return intCode;
-    }
 
 }
--- src/test/java/be/annelyse/Day2Test.java
+++ src/test/java/be/annelyse/Day2Test.java
@@ -3,6 +3,7 @@ package be.annelyse;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,26 +11,26 @@ import java.util.List;
 class Day2Test {
 
     @Test
-    void solvePart1() {
-        List<Integer> input1 = Arrays.asList(1,0,0,0,99);
-        List<Integer> output1 = Arrays.asList(2,0,0,0,99);
-        Assertions.assertEquals(output1, Day2.solvePart1(input1));
+    void solvingAlgorithm() {
+        List<Integer> input1 = Arrays.asList(1, 0, 0, 0, 99);
+        List<Integer> output1 = Arrays.asList(2, 0, 0, 0, 99);
+        Assertions.assertEquals(output1, Day2.solvingAlgoritm(input1));
 
-        List<Integer> input2 = Arrays.asList(2,3,0,3,99);
-        List<Integer> output2 = Arrays.asList(2,3,0,6,99);
-        Assertions.assertEquals(output2, Day2.solvePart1(input2));
+        List<Integer> input2 = Arrays.asList(2, 3, 0, 3, 99);
+        List<Integer> output2 = Arrays.asList(2, 3, 0, 6, 99);
+        Assertions.assertEquals(output2, Day2.solvingAlgoritm(input2));
 
-        List<Integer> input3 = Arrays.asList(2,4,4,5,99,0);
-        List<Integer> output3 = Arrays.asList(2,4,4,5,99,9801);
-        Assertions.assertEquals(output3, Day2.solvePart1(input3));
+        List<Integer> input3 = Arrays.asList(2, 4, 4, 5, 99, 0);
+        List<Integer> output3 = Arrays.asList(2, 4, 4, 5, 99, 9801);
+        Assertions.assertEquals(output3, Day2.solvingAlgoritm(input3));
 
         List<Integer> input4 = Arrays.asList(1,1,1,4,99,5,6,0,99);
         List<Integer> output4 = Arrays.asList(30,1,1,4,2,5,6,0,99);
-        Assertions.assertEquals(output4, Day2.solvePart1(input4));
-
+        Assertions.assertEquals(output4, Day2.solvingAlgoritm(input4));
     }
 
     @Test
-    void solvePart2() {
+    void solvePart1() throws IOException {
+        Assertions.assertEquals(3895705, Day2.solvePart1(Day2.getIntCodeInput(),1202));
     }
 }
